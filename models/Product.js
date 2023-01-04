31a0386020e666cbbd78a735c27d6ab64ed66bd4
1-those-jacks <85271008+1-those-jacks@users.noreply.github.com>
2023-01-03 23:28:39 -0600

    Created Models.

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 // import important parts of sequelize library
+const { DECIMAL } = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
@@ -9,7 +10,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define id column
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		// define product name column
+		product_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		// define price column
+		price: {
+			DataTypes: DECIMAL(10,2),
+			allowNull: false,
+			validate: {isDecimal: true}
+		},
+		// define stock column
+		stock: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 10,
+			validate: {isNumeric: true}
+		},
+		// define category id column
+		category_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'category',
+				key: 'id'
+			}
+		}
   },
   {
     sequelize,
